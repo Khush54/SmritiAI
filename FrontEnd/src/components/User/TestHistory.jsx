5a666328719db0e845843c5faac9b3636dd5b7af
Khush54 <khushkaur023@gmail.com>
2026-05-20 13:17:31 +0530

    Error Fixed

--- FrontEnd/src/components/User/TestHistory.jsx
+++ FrontEnd/src/components/User/TestHistory.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { getPatientAssessments } from '../../services/assessmentService';
+import { getPatientAssessments } from '../../Services/assessmentService';
 import { Bar } from 'react-chartjs-2';
 import {
     Chart as ChartJS,
